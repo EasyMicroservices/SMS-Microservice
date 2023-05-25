c695aaf8a667c3c02810dfff8c515ac649bff00a
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-25 15:40:10 +0330

    add message type

---
+++ src/CSharp/EasyMicroservices.SMSMicroservice.Domain/DataTypes/MessageStatusType.cs
@@ -0,0 +1,17 @@
+﻿namespace EasyMicroservices.SMSMicroservice.DataTypes
+{
+    public enum MessageStatusType : byte
+    {
+        None = 0,
+        Default = 1,
+        All = 2,
+        Other = 3,
+        Unknown = 4,
+        Nothing = 5,
+        Queue = 6,
+        Sending = 7,
+        Sent = 8,
+        Exception = 9,
+        Canceled = 10
+    }
+}
